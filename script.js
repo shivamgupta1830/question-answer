36c671d384e2a2c70ccb556aa741e00f860cccb9
shivamgupta1830 <shivamgupta1830@gmail.com>
2024-02-23 14:46:52 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const btn = document.querySelectorAll(".btn");
+const ans = document.querySelectorAll(".answer");
+const ques = document.querySelectorAll(".question");
+const box = document.querySelectorAll(".question-box");
+
+// Method-1 #################################################
+
+btn.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const text = button.innerHTML;
+    if (text === "↓") {
+      button.innerHTML = "↑";
+    } else {
+      button.innerHTML = "↓";
+    }
+
+    const question_box = e.currentTarget.parentElement.parentElement;
+    const answer = question_box.children[1];
+    answer.classList.toggle("show");
+  });
+});
+
+// Method-2 #################################################
+
+// box.forEach((box) => {
+//   const button = box.querySelector(".btn");
+//   const answer = box.querySelector(".answer");
+//   console.log(button, answer);
+
+//   button.addEventListener("click", () => {
+//     const text = button.innerHTML;
+//     if (text === "+") {
+//       button.innerHTML = "-";
+//     } else {
+//       button.innerHTML = "+";
+//     }
+
+//     answer.classList.toggle("show");
+//   });
+// });
